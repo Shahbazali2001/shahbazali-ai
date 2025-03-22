ba93656651c2fa578fa26e2dffd3b26e27655c3c
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-23 01:10:01 +0500

    Tailwind CSS configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-
+  
 export default function Home() {
   return (
-        <main className="relative">
-          <div>
-            <h1>Hello Portfolio</h1>
+        <main 
+        className="relative bg-black-100 flex justify-center items-center flex-col 
+        overflow-hidden mx-auto sm:px-10 px-5">
+          <div className="max-w-7xl w-full ">
+            <h1 className="font-extrabold sm:text-[64px] text-[44px] text-white text-center" >Hello Portfolio</h1>
           </div>
         </main>
   );
