40f9291c0c4ee0ddbe34a6618e473990a47a7947
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-22 23:47:03 +0500

    Tailwind CSS Configured

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,28 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  purge: {
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", 
+              "./components/**/*.{js,ts,jsx,tsx}",
+              "./app/**/*.{js,ts,jsx,tsx}"],
+          
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        black:{
+          DEFAULT : "#000000",
+          100 : "#000319",
+        },
+        white:{
+          DEFAULT : "#ffffff",
+        }
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+
 };
 
 export default config;
