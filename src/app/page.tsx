53d0c7d7871481603dfc3b51d80dc6517fee8f20
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-23 20:05:42 +0500

    Minor Updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import Hero from "@/components/Hero";
+
   
 export default function Home() {
   return (
-        <main 
-        className="relative bg-black-100 flex justify-center items-center flex-col 
-        overflow-hidden mx-auto sm:px-10 px-5">
-          <div className="max-w-7xl w-full ">
-            <h1 className="font-extrabold sm:text-[64px] text-[44px] text-white text-center" >Hello Portfolio</h1>
-          </div>
-        </main>
+        <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+          <div className="max-w-7xl w-full">
+           <h1>Hello Portfolio</h1>
+           <Hero />
+          </div>          
+        </main> 
   );
 }
