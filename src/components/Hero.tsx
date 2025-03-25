f8332e990f4f0d7ab6a0c40ea95b506e667b3013
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-26 00:05:14 +0500

    Minor Changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
       </div>
 
       <div>
-        <div className="relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+        <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black-100">
           <div
             className={cn(
               "absolute inset-0",
@@ -29,10 +29,8 @@ const Hero = () => {
             )}
           />
           {/* Radial gradient for the container to give a faded look */}
-          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-          <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-            Backgrounds
-          </p>
+          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
+          
         </div>
       </div>
     </div>
